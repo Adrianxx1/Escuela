943a1ce7d1d849cf03ecd733caa30de8b2835704
Luis Adrian Amador Martínez <triaxis12q@gmail.com>
2026-06-10 11:43:08 -0600

    Creacion del modulo curso

--- src/main/java/com/adrian/escuela/entities/Curso.java
+++ src/main/java/com/adrian/escuela/entities/Curso.java
@@ -1,5 +1,7 @@
 package com.adrian.escuela.entities;
 
+import com.adrian.escuela.utils.StringCustomUtils;
+import com.adrian.escuela.utils.ValoresNumericosUtils;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -28,8 +30,26 @@ public class Curso {
     private Integer creditos;
 
 
-
     @Builder.Default
     @OneToMany(mappedBy = "curso")
     private List<Grupo> grupos = new ArrayList<>();
+
+    public void actualizar(String nombre, String descripcion, Integer creditos) {
+        validarDatos(nombre, descripcion, creditos);
+        this.nombre = nombre.trim();
+        this.descripcion = descripcion != null ? descripcion.trim() : null;
+        this.creditos = creditos;
+    }
+
+    private void validarDatos(String nombre, String descripcion, Integer creditos) {
+        StringCustomUtils.validarTamanio(nombre.trim(), 4, 100,
+                "El nombre es requerido y debe tener entre 4 y 100 caracteres");
+
+        if (descripcion != null)
+            StringCustomUtils.validarTamanio(descripcion.trim(), 1, 200,
+                    "La descripción debe tener máximo 200 caracteres");
+
+        ValoresNumericosUtils.validarEnteroPositivo(creditos,
+                "Los créditos deben ser mayor a 0");
+    }
 }
--- src/main/java/com/adrian/escuela/repositories/GrupoRepository.java
+++ src/main/java/com/adrian/escuela/repositories/GrupoRepository.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
-    boolean existsByMaestroId(Long idMaestro
-    );
+    boolean existsByMaestroId(Long idMaestro);
     boolean existsByAulaId(Long idAula);
+    boolean existsByCursoId(Long cursoId);
     boolean existsByCurso_IdAndMaestro_IdAndAula_IdAndPeriodo(
-           Long cursoId,
-           Long maestroId,
-           Long aulaId,
-           String periodo
+            Long cursoId,
+            Long maestroId,
+            Long aulaId,
+            String periodo
     );
 }
